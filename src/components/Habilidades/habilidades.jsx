decb25c27d99c01a21a79ce140a4217831af565d
breno.ferrari <breno.ferrari@fiap.com.br>
2024-09-27 17:56:05 -0300

    Ajustes gerais

--- src/components/Habilidades/habilidades.jsx
+++ src/components/Habilidades/habilidades.jsx
@@ -5,17 +5,17 @@ import { useEffect, useRef } from "react"
 import { motion } from "framer-motion";
 import { useInView } from "framer-motion";
 
-export default function Habilidades() { 
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
+export default function Habilidades() {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
     return (
-      <section className={styles.habilidades} id="habilidades" ref={ref}>
+        <section className={styles.habilidades} id="habilidades" ref={ref}>
             <div className={styles.habilidades__container}>
-                <div className={styles.habilidades__container__left}style={{
+                <div className={styles.habilidades__container__left} style={{
                     transform: isInView ? "none" : "translateX(-200px)",
                     opacity: isInView ? 1 : 0,
                     transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
-                    }}>
+                }}>
                     <p className={styles.habilidades__container__left__text}>
                         Essas são as minhas
                     </p>
@@ -24,25 +24,25 @@ export default function Habilidades() {
                     </p>
                 </div>
                 <div className={styles.habilidades__container__rigth} style={{
-                      transform: isInView ? "none" : "translateX(200px)",
-                      opacity: isInView ? 1 : 0,
-                      transition: "all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
-                    }}>
-                {isInView? Tec.map((item,index)=>(
-                    <motion.div
-                    className={styles.habilidades__container__rigth__bar} key={index}
-                    >
-                        <div className={styles.habilidades__container__rigth__bar__stacks}>
-                            <p className={styles.habilidades__container__rigth__bar__stacks__stack}>{item.Icon}</p>
-                            <p className={styles.habilidades__container__rigth__bar__stacks__stack}>{item.Title}</p>
-                        </div>  
-                        <ToggleSlide value={item.Know}/>
-                    </motion.div>
-                )):""}
+                    transform: isInView ? "none" : "translateX(200px)",
+                    opacity: isInView ? 1 : 0,
+                    transition: "all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+                }}>
+                    {isInView ? Tec.map((item, index) => (
+                        <motion.div className={styles.habilidades__container__rigth__bar} key={index}>
+                            <div className={styles.habilidades__container__rigth__bar__stacks}>
+                                <p className={styles.habilidades__container__rigth__bar__stacks__stack} style={{ color: item.color }}>
+                                    {item.Icon}
+                                </p>
+                                <p className={styles.habilidades__container__rigth__bar__stacks__stack}>{item.Title}</p>
+                            </div>
+                            <ToggleSlide value={item.Know} color={item.color} />
+                        </motion.div>
+
+                    )) : ""}
                 </div>
-                
+
             </div>
-      </section>
+        </section >
     )
-  }
-  
+}
--- src/components/Habilidades/progressBar/toggle.jsx
+++ src/components/Habilidades/progressBar/toggle.jsx
@@ -2,26 +2,27 @@ import { useEffect, useRef } from "react";
 import styles from "./toggle.module.scss"
 
 
-export default function ToggleSlide({children,value}){
+export default function ToggleSlide({ children, value, color }) {
   const ref = useRef(null)
   const effectRan = useRef(false)
 
   useEffect(() => {
-    if(effectRan.current === false){
+    if (effectRan.current === false) {
       let bar = ref.current
       bar?.style.setProperty(
         "--bg-size",
-          value ? value+"%" : "#fff"
+        value ? value + "%" : "#fff"
       );
-      return() =>{
-          effectRan.current = true
-      } 
-  }
-  });
+      bar?.style.setProperty("--toggle-after-bg", color);
+      return () => {
+        effectRan.current = true
+      }
+    }
+  }, [value, color])
 
-  return ( 
+  return (
     <div className={styles.container} id="slider">
-       <div className={styles.container__slider} ref={ref} value={value}>{children}</div>
+      <div className={styles.container__slider} ref={ref} value={value}>{children}</div>
     </div>
   )
 }
